8a2e69fcf06d5c34582c76173a8ce4b8d6280f2b
khawla-1006 <khawla.ben.in@gmail.com>
2024-08-15 00:28:03 +0200

    status 204

--- index.js
+++ index.js
@@ -31,7 +31,14 @@ app.get('/api/notes/:id', (request, response) => {
     response.send("note not found!")
 
   }
-}) 
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id != id)
+  response.status(204).end()
+})
+
 
 const PORT = 3001
 app.listen(PORT)
